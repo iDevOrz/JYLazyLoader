1602c2c99029a2189158b2171e831ed689df3b05
DevZhang <me@devorz.com>
2019-08-02 16:30:37 +0800

    Initial Commit

---
+++ JYLazyLoader/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  JYLazyLoader
+//
+//  Created by 张建宇 on 2019/8/2.
+//  Copyright © 2019 DevOrz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
